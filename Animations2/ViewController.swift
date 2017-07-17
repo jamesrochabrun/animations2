58c0276f5329a3fb5f5bf471fcc9f9e7f5f8189e
james <jamesrochabrun@gmail.com>
2017-07-16 21:22:46 -0700

    update

--- Animations2/ViewController.swift
+++ Animations2/ViewController.swift
@@ -12,9 +12,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Util
     var isNewImage: Bool = false
     var showEffects: Bool = false
     
+    //MARK: UI
     lazy var snowClipView: UIView = {
        let snowClipView = UIView(frame: self.view.frame.offsetBy(dx: 0, dy: 50))
         let snowView = SnowView(frame: CGRect(x: -150, y:-100, width: 300, height: 50))
@@ -23,15 +25,23 @@ class ViewController: UIViewController {
         snowClipView.addSubview(snowView)
         return snowClipView
     }()
-    
-    
     @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    @IBOutlet weak var departureLabel: UILabel!
+    
+    @IBOutlet weak var arrivalLabel: UILabel!
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
   
         backgroundImageView.addSubview(snowClipView)
     }
+}
 
+//MARK: animating using the center of a view
+extension ViewController {
     
     @IBAction func fadeIn(_ sender: Any) {
         
@@ -40,6 +50,29 @@ class ViewController: UIViewController {
         let weatherImage = isNewImage ? #imageLiteral(resourceName: "bg-snowy") : #imageLiteral(resourceName: "bg-sunny")
         fade(toImage: weatherImage)
         fadeSnowEffect(self.showEffects)
+        
+        if showEffects {
+            departureLabel.text = "PUQ"
+            arrivalLabel.text = "MIA"
+            statusLabel.text = "ARRIVED"
+        
+        } else {
+            departureLabel.text = "MIA"
+            arrivalLabel.text = "PUQ"
+            statusLabel.text = "DEPARTED"
+
+        }
+        
+        let offSetDeparting = CGPoint(
+            x: showEffects ? -80.0 : 80.0,
+            y: 0.0)
+        moveLabel(departureLabel, offSet: offSetDeparting)
+        
+        let offSetArriving = CGPoint(
+            x: 0.0,
+            y: showEffects ? 50.0 : -50.0)
+        moveLabel(arrivalLabel, offSet: offSetArriving)
+        cubeTransition(label: statusLabel)
     }
     
     //MARK: helper methods
@@ -77,11 +110,97 @@ class ViewController: UIViewController {
     }
 }
 
+//MARK: animating labels by transformation
+extension ViewController {
+    
+    //MARK: Helepr method
+    func moveLabel(_ label: UILabel,  offSet: CGPoint) {
+        
+        //create & setup helper label
+        let auxLabel = duplicateLabel(label: label)
+        auxLabel.transform = CGAffineTransform(translationX: offSet.x, y: offSet.y)
+        auxLabel.alpha = 0
+        view.addSubview(auxLabel)
+        //Fade out & translate real label
+        UIView.animate(withDuration: 0.5,
+                       delay: 0.0,
+                       options: .curveEaseIn, animations: {
+                        label.transform = CGAffineTransform(translationX: offSet.x, y: offSet.y)
+                        label.alpha = 0
+        }, completion: nil)
+        
+        //Fade in & translate helper label
+        UIView.animate(withDuration: 0.25,
+                       delay: 0.2,
+                       options: .curveEaseIn, animations: { 
+                        auxLabel.transform = .identity
+                        auxLabel.alpha = 1.0
+        }, completion: { _ in
+            //Update real label & remove helper label
+            label.alpha = 1.0
+            label.transform = .identity
+            auxLabel.removeFromSuperview()
+        })
+    }
+    
+    func cubeTransition(label: UILabel) {
+        
+        //Create & set up a helper label
+        let auxLabel = duplicateLabel(label: label)
+        
+        let auxLabelOffset = label.frame.size.height/2.0
+        
+        let scale = CGAffineTransform(
+            scaleX: 1.0,
+            y: 0.1
+        )
+        let translate = CGAffineTransform(
+            translationX: 0.0,
+            y: auxLabelOffset
+        )
+        
+        auxLabel.transform = scale.concatenating(translate)
+        
+        label.superview?.addSubview(auxLabel)
+        
+        UIView.animate(
+            withDuration: 0.5,
+            delay: 0.0,
+            options: .curveEaseOut,
+            animations: {
+                //Scale and translate the helper label down
+                auxLabel.transform = .identity
+                //Scale and translate the real label up
+                label.transform = scale
+                    .concatenating(translate.inverted())
+        },
+            completion: {_ in
+                //Update the real label's text and reset its transform
+                label.text = auxLabel.text
+                label.transform = .identity
+                
+                //Remove the helper label
+                auxLabel.removeFromSuperview()
+        }
+        )
+    }
+}
+
+
 extension ViewController {
     
     //MARK: utility methods
     func delay(seconds: Double, completion: @escaping ()-> Void) {
         DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: completion)
+    }
+    
+    func duplicateLabel(label: UILabel) -> UILabel {
+        let auxLabel = UILabel(frame: label.frame)
+        auxLabel.font = label.font
+        auxLabel.textAlignment = label.textAlignment
+        auxLabel.textColor = label.textColor
+        auxLabel.backgroundColor = label.backgroundColor
+        return auxLabel
     }
 }
 
